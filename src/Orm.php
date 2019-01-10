b24fb7284958705c801b2b8ed8cc7bf4222c1203
TJ Draper <tj@buzzingpixel.com>
2019-01-10 15:59:23 -0600

    Fix a bug where Orm::new did not return an instance of Orm

--- src/Orm.php
+++ src/Orm.php
@@ -10,7 +10,34 @@ declare(strict_types=1);
 namespace corbomite\db;
 
 use Atlas\Orm\Atlas;
+use Atlas\Table\TableLocator;
+use Atlas\Pdo\ConnectionLocator;
+use \Atlas\Mapper\MapperLocator;
+use Atlas\Mapper\MapperQueryFactory;
+use Atlas\Orm\Transaction\AutoCommit;
+use Atlas\Orm\Transaction\Transaction;
 
 class Orm extends Atlas
 {
+    public static function new(...$args): Atlas
+    {
+        $transactionClass = AutoCommit::CLASS;
+
+        $end = end($args);
+        if (is_string($end) && is_subclass_of($end, Transaction::CLASS)) {
+            $transactionClass = array_pop($args);
+        }
+
+        $connectionLocator = ConnectionLocator::new(...$args);
+
+        $tableLocator = new TableLocator(
+            $connectionLocator,
+            new MapperQueryFactory()
+        );
+
+        return new Orm(
+            new MapperLocator($tableLocator),
+            new $transactionClass($connectionLocator)
+        );
+    }
 }

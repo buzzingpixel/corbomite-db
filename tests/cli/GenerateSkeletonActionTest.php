6008ef4eacacd08f6dc81e3a9eace0fb06155562
TJ Draper <tj@buzzingpixel.com>
2019-02-27 21:56:51 -0600

    Add tests for GenerateSkeletonAction class

---
+++ tests/cli/GenerateSkeletonActionTest.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace corbomite\tests;
+
+use PHPUnit\Framework\TestCase;
+use corbomite\db\cli\SkeletonCliGenerator;
+use corbomite\db\cli\GenerateSkeletonAction;
+
+class GenerateSkeletonActionTest extends TestCase
+{
+    public function testAttribute()
+    {
+        $generator = $this->createMock(SkeletonCliGenerator::class);
+
+        $generator->expects(self::once())
+            ->method('__invoke')
+            ->willReturn(null);
+
+        $generateSkeletonAction = new GenerateSkeletonAction($generator);
+
+        self::assertNull($generateSkeletonAction());
+    }
+}
